59843ce95c378e1773a2180e84d83ec7a4866ba5
Paul Caballero <paulcaballero30@yahoo.com>
2023-02-08 16:07:44 +0800

    update-for-Q5-and-Q6

--- flows/03_deployments/parameterized_flow.py
+++ flows/03_deployments/parameterized_flow.py
@@ -25,8 +25,8 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix some dtype issues"""
-    df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-    df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'])
+    df['lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'])
     print(df.head(5))
     print(f'columns: {df.dtypes}')
     print(f'rows: {len(df)}')
@@ -63,7 +63,7 @@ def etl_parent_flow(months: list[int] = [1, 2], year: int = 2021, color: str = '
         etl_web_to_gcs(year, month, color)
 
 if __name__ == '__main__':
-    color = 'yellow'
-    months = [2, 3]
+    color = 'green'
+    months = [4]
     year = 2019
     etl_parent_flow(months, year, color)
